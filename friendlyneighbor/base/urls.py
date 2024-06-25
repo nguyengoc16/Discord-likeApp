64e920d358a632a8eef7909b7f83c0fb6826c295
nguyengoc16 <92981256+nguyengoc16@users.noreply.github.com>
2024-06-25 17:37:16 +0700

    [FEAT] user login, logout

--- friendlyneighbor/base/urls.py
+++ friendlyneighbor/base/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
     path('rooms/<str:pk>/',views.room, name = "room"),
     path('create-room/', views.create_room, name="create_room"),
     path('update-room/<int:pk>', views.update_room, name="update_room"),
--- friendlyneighbor/base/views.py
+++ friendlyneighbor/base/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
+def loginPage(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(password)
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "No user found")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password not right")
+    context ={}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
