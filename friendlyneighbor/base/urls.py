a170562fc71384d92743eec52f76af296fac21a2
nguyengoc16 <92981256+nguyengoc16@users.noreply.github.com>
2024-06-25 15:41:28 +0700

    Init project

---
+++ friendlyneighbor/base/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('rooms/<str:pk>/',views.room, name = "room"),
+    path('create-room/', views.create_room, name="create_room"),
+    path('update-room/<int:pk>', views.update_room, name="update_room"),
+    path('delete-room/<int:pk>', views.delete_room, name="delete_room"),
+
+    
+]
