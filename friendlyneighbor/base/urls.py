2a71334db6bb34f8f340493495f327ee7422e5a9
nguyengoc16 <92981256+nguyengoc16@users.noreply.github.com>
2024-06-25 21:43:46 +0700

    [FEAT] user registration

--- friendlyneighbor/base/urls.py
+++ friendlyneighbor/base/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('register/', views.registerUser, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('rooms/<str:pk>/',views.room, name = "room"),
--- friendlyneighbor/base/views.py
+++ friendlyneighbor/base/views.py
@@ -5,12 +5,14 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
 
 def loginPage(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('home')
@@ -32,12 +34,30 @@ def loginPage(request):
             return redirect('home')
         else:
             messages.error(request, "Username or password not right")
-    context ={}
+    context ={'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm()
+    context ={'page': page,
+              'form':form}
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
